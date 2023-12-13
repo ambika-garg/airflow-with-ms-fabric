f12dfcc331c66e8bcbd74b87ca5aa933d0744c09
ambika-garg <70703123+ambika-garg@users.noreply.github.com>
2023-12-13 14:20:52 -0500

    Comment out private package dag

--- dags/private-package.py
+++ dags/private-package.py
@@ -1,11 +1,11 @@
-from airflow_operator.hello_operator import SampleCustomOperator # This is private package import
-from airflow import DAG
+# from airflow_operator.hello_operator import SampleCustomOperator # This is private package import
+# from airflow import DAG
 
 
-with DAG(
-   "tutorial",
-   tags=["example"],
-) as dag:
-    sample_task = SampleCustomOperator(task_id="sample-task", name="foo_bar")
+# with DAG(
+#    "tutorial",
+#    tags=["example"],
+# ) as dag:
+#     sample_task = SampleCustomOperator(task_id="sample-task", name="foo_bar")
 
-    sample_task
+#     sample_task
