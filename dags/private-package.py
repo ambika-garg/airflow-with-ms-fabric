966806ebe546a25fc0aa85153fcef85aaa2d6196
ambika-garg <70703123+ambika-garg@users.noreply.github.com>
2023-12-13 14:03:30 -0500

    Add files to support private package requirement

---
+++ dags/private-package.py
@@ -0,0 +1,11 @@
+from airflow_operator.hello_operator import SampleCustomOperator # This is private package import
+from airflow import DAG
+
+
+with DAG(
+   "tutorial",
+   tags=["example"],
+) as dag:
+    sample_task = SampleCustomOperator(task_id="sample-task", name="foo_bar")
+
+    sample_task
