66db804aa1fba54c41037ef9c13b82e337cea5e4
Ambika Garg <v-ambikagarg@microsoft.com>
2023-12-29 14:02:26 -0500

    created new package requirement

--- dags/use-private-package.py
+++ dags/use-private-package.py
@@ -1,19 +1,29 @@
-# from airflow_operator.hello_operator import SampleOperator
-from airflow.operators.bash import BashOperator
+from airflow_operators.sampleOperator import SampleOperator
+from datetime import datetime
 from airflow import DAG
-
+from airflow.operators.bash import BashOperator
 
 with DAG(
-   "tutorial",
-   tags=["example"],
+    "create_private_package_as_requirement",
+    default_args={
+        "depends_on_past": False,
+        "email": ["airflow@example.com"],
+        "email_on_failure": False,
+        "email_on_retry": False,
+        "retries": 1,
+    },
+    description="A simple tutorial DAG",
+    schedule_interval=None,
+    start_date=datetime(2021, 1, 1),
+    catchup=False,
+    tags=["requirement"],
 ) as dag:
 
     list_requirements = BashOperator(
         task_id="list_requirements",
-        bash_commadn = "pip list"
+        bash_command = "pip list"
     )
 
-    # sample_task = SampleOperator(task_id="sample-task", name="foo_bar")
+    sample_task = SampleOperator(task_id="sample-task", name="foo_bar")
 
-    list_requirements
-    # >> sample_task
+    list_requirements >> sample_task
